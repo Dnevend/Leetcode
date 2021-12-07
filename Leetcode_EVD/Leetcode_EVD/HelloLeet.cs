9070cd1dfb74c8911effc8255dbae03a85dc654e
Dnevend <961427439@qq.com>
2021-12-07 21:27:20 +0800

    HelloLeetBug

---
+++ Leetcode_EVD/Leetcode_EVD/HelloLeet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_EVD
+{
+    class HelloLeet
+    {
+
+        public static void Hello()
+        {
+            Console.WriteLine("LeetBug");
+        }
+
+    }
+}
---
+++ Leetcode_EVD/Leetcode_EVD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Leetcode_EVD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloLeet.Hello();
+            Console.ReadLine();
+        }
+    }
+}
