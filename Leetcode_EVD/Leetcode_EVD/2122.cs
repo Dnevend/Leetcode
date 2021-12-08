7df13a79469386d08d3f9cd4380b66cb96530a0a
Dnevend <961427439@qq.com>
2021-12-08 20:26:11 +0800

    2122

---
+++ Leetcode_EVD/Leetcode_EVD/2122.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_EVD
+{
+    class _2122
+    {
+
+        public int PurchasePlans(int[] nums, int target)
+        {
+
+            int planCount = 0;
+
+            int MOD = 1000000007;
+
+            Array.Sort(nums);
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[left] + nums[right] > target)
+                {
+                    right--;
+                }
+                else {
+                    planCount = (planCount + (right - left)) % MOD;
+                    left++;
+                }
+            }
+
+            return planCount;
+
+        }
+
+    }
+}
