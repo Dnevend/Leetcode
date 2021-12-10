d1bd4a59b55563b251c024e8e69839fcb6e97225
Dnevend <961427439@qq.com>
2021-12-10 20:50:19 +0800

    1600

---
+++ Leetcode_EVD/Leetcode_EVD/EVD/1600.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_EVD.EVD
+{
+    class _1600
+    {
+
+
+    }
+
+    /// <summary>
+    /// https://leetcode-cn.com/problems/design-parking-system/
+    /// </summary>
+    public class ParkingSystem
+    {
+
+        int bigSize, mediumSize, smallSize;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.bigSize = big;
+            this.mediumSize = medium;
+            this.smallSize = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            switch (carType)
+            {
+                case 1:
+                    if (--bigSize < 0)
+                    {
+                        return false;
+                    }
+                    break;
+                case 2:
+                    if (--mediumSize < 0)
+                    {
+                        return false;
+                    }
+                    break;
+                case 3:
+                    if (--smallSize < 0)
+                    {
+                        return false;
+                    }
+                    break;
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Your ParkingSystem object will be instantiated and called as such:
+     * ParkingSystem obj = new ParkingSystem(big, medium, small);
+     * bool param_1 = obj.AddCar(carType);
+     */
+
+}
