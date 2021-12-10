57b0cefc9947b7e436fe59e7dd614d5002f526e6
Dnevend <961427439@qq.com>
2021-12-10 21:05:44 +0800

    1600_update

--- Leetcode_EVD/Leetcode_EVD/EVD/1600.cs
+++ Leetcode_EVD/Leetcode_EVD/EVD/1600.cs
@@ -18,25 +18,43 @@ namespace Leetcode_EVD.EVD
     public class ParkingSystem
     {
 
+        // way 1
         //int bigSize, mediumSize, smallSize;
-        int[] typeSize = new int[4];
+
+        // way 2
+        //int[] typeSize = new int[4];
+
+        int[] typeSize;
 
         public ParkingSystem(int big, int medium, int small)
         {
+            // way 1
             //this.bigSize = big;
             //this.mediumSize = medium;
             //this.smallSize = small;
-            typeSize[1] = big;
-            typeSize[2] = medium;
-            typeSize[3] = small;
+
+            // way 2
+            //typeSize[1] = big;
+            //typeSize[2] = medium;
+            //typeSize[3] = small;
+
+            // way 3
+            typeSize = new int[] { 0, big, medium, small };
+
         }
 
         public bool AddCar(int carType)
         {
-            if (--typeSize[carType] < 0)
-            {
-                return false;
-            }
+            // way 3
+            return --typeSize[carType] >= 0;
+
+            // way 2
+            //if (--typeSize[carType] < 0)
+            //{
+            //    return false;
+            //}
+
+            // way 1
             //switch (carType)
             //{
             //    case 1:
@@ -58,7 +76,8 @@ namespace Leetcode_EVD.EVD
             //        }
             //        break;
             //}
-            return true;
+
+            //return true;
         }
     }
 
