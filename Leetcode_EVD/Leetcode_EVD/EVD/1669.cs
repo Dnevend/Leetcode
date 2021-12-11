58a569d0ff85f3d42841b75a3c693409be6d9067
Dnevend <961427439@qq.com>
2021-12-11 11:29:46 +0800

    1669_update

--- Leetcode_EVD/Leetcode_EVD/EVD/1669.cs
+++ Leetcode_EVD/Leetcode_EVD/EVD/1669.cs
@@ -19,18 +19,25 @@ namespace Leetcode_EVD.EVD
         {
 
             int max = 0;
-            int tmp;
-            for (var i = 0; i < accounts.Length; i++)
+            // way 1
+            //int tmp;
+            //for (var i = 0; i < accounts.Length; i++)
+            //{
+            //    tmp = 0;
+            //    for (var j = 0; j < accounts[i].Length; j++)
+            //    {
+            //        tmp += accounts[i][j];
+            //    }
+            //    if (tmp > max)
+            //    {
+            //        max = tmp;
+            //    }
+            //}
+
+            // way 2
+            for (int i = 0; i < accounts.Length; i++)
             {
-                tmp = 0;
-                for (var j = 0; j < accounts[i].Length; j++)
-                {
-                    tmp += accounts[i][j];
-                }
-                if (tmp > max)
-                {
-                    max = tmp;
-                }
+                max = Math.Max(accounts[i].Sum(), max)
             }
 
             return max;
