a43a05436c4da6e1fa7a5e3a0485bbbe0e72dd31
Dnevend <961427439@qq.com>
2021-12-11 11:10:50 +0800

    1669

---
+++ Leetcode_EVD/Leetcode_EVD/EVD/1669.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_EVD.EVD
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/richest-customer-wealth/
+    /// </summary>
+    class _1669
+    {
+    }
+
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+
+            int max = 0;
+            int tmp;
+            for (var i = 0; i < accounts.Length; i++)
+            {
+                tmp = 0;
+                for (var j = 0; j < accounts[i].Length; j++)
+                {
+                    tmp += accounts[i][j];
+                }
+                if (tmp > max)
+                {
+                    max = tmp;
+                }
+            }
+
+            return max;
+        }
+    }
+
+}
