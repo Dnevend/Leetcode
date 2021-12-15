0c568f9ef2808fa9afd4e8658e7faa299fc09ba6
Dnevend <961427439@qq.com>
2021-12-15 21:23:58 +0800

    383

---
+++ Leetcode_EVD/Leetcode_EVD/EVD/383.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_EVD.EVD
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/ransom-note/
+    /// </summary>
+    class _383
+    {
+
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            int[] magazines = new int[26];
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                magazines[magazine[i] - 'a']++;
+            }
+
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+
+                magazines[ransomNote[i] - 'a']--;
+
+            }
+
+            for (int i = 0; i < magazines.Length; i++)
+            {
+                if(magazines[i] > 0) { return false; }
+            }
+
+            return true;
+        }
+
+    }
+}
