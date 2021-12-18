5c4c037973f6bc0287d7db9453fc8a69ad5176bf
Dnevend <961427439@qq.com>
2021-12-18 12:58:17 +0800

    88

--- src/88/index.js
+++ src/88/index.js
@@ -5,32 +5,34 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-	var len = m + n;
-	for (var i = n - 1; i >= 0; i--) {
-	  if (nums2[i] >= nums1[len - i - 2]) {
-		nums1[len - i - 1] = nums2[i];
-	  } else {
-		swich(nums1, len - i - 2, nums2[i]);
-	  }
-	}
-  };
-  
-  var swich = function (nums, index, value) {
-	for (var i = index - 1; i > 0; i--) {
-	  if (value >= nums[i] && i > -1) {
-		nums[i + 2] = nums[i + 1];
-		nums[i + 1] = value;
-		return;
-	  } else {
-		nums[i + 2] = nums[i + 1];
-		nums[i + 1] = nums[i];
-		swich(nums, i, value);
-		return;
-	  }
-	}
-  };
-  
+// 思路：从末尾开始进行比较替换
+// 未成功
+var merge = function (nums1, m, nums2, n) {
+  var len = m + n;
+  for (var i = n - 1; i >= 0; i--) {
+    if (nums2[i] >= nums1[len - i - 2]) {
+      nums1[len - i - 1] = nums2[i];
+    } else {
+      swich(nums1, len - i - 2, nums2[i]);
+    }
+  }
+};
+
+var swich = function (nums, index, value) {
+  console.log(nums);
+  for (var i = index - 1; i > 0; i--) {
+    if (value >= nums[i] && i > -1) {
+      nums[i + 2] = nums[i + 1];
+      nums[i + 1] = value;
+      return;
+    } else {
+      nums[i + 2] = nums[i + 1];
+      nums[i + 1] = nums[i];
+      swich(nums, i, value);
+      return;
+    }
+  }
+};
 
 var tmp1 = [1, 2, 3, 0, 0, 0];
 var l1 = 3;
--- src/88/second.js
+++ src/88/second.js
@@ -5,14 +5,16 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-    
-    var j = 0;
-    for(var i = m; i <= n+n; i++)
-    {
-        nums1[i] = nums2[j++];
-    }
-    nums1;
+var merge = function (nums1, m, nums2, n) {
+  // 将nums2填充至num1末尾
+  nums1.splice(m, nums1.length - m, ...nums2)
+  // num1排序
+  nums1.sort((a, b) => a - b);
+};
 
-  };
-  
+var tmp1 = [1, 2, 3, 0, 0, 0];
+var l1 = 3;
+var tmp2 = [2, 5, 6];
+var l2 = 3;
+merge(tmp1, l1, tmp2, l2);
+console.log(tmp1);
